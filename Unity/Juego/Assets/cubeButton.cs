202e69c78c1ca536760fb1268b0cc92207dc9f03
Guillaume Mercadal <guillaume218.mercadal@gmail.com>
2019-11-27 15:04:03 +0100

    test button spawn object

---
+++ Unity/Juego/Assets/cubeButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeButton : MonoBehaviour
+{
+    public GameObject cubeb;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject test = Instantiate(cubeb, transform.position + (transform.forward * 2), transform.rotation);
+            test.transform.localScale = new Vector3(100, 100, 100);
+        }
+    }
+}
