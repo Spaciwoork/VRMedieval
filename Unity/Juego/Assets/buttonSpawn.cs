9125ab9a5ce96a9444c9503c23dbaab02c3f8923
Guillaume Mercadal <guillaume218.mercadal@gmail.com>
2019-11-27 14:14:08 +0100

    test button spawn object

---
+++ Unity/Juego/Assets/buttonSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonSpawn : MonoBehaviour
+{
+    public GameObject ball;
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        GameObject test = Instantiate(ball, transform.position + (transform.forward * 2), transform.rotation);
+        test.transform.localScale = new Vector3(100, 100, 100);
+    }
+}
