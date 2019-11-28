eb8c8f38b2a4062c6b140c96f9ae6b303aa371ab
Paul Huguet <p.huguet@horizonprevention.fr>
2019-11-28 18:12:50 +0100

    keorke

---
+++ Unity/Juego/Assets/Script/Gaze.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Gaze : MonoBehaviour
+{
+    public float sightlength = 100f;
+    public GameObject selectedObj;
+    public float hoverforwardDistance = .5f;
+    [SerializeField] private GameObject bookRecipes;
+    public GameObject exitButton;
+
+    void Start () {
+            bookRecipes.SetActive(false);
+            exitButton.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+      RaycastHit seen;
+      Ray raydirection = new Ray(transform.position, transform.forward);
+      if (Physics.Raycast(raydirection, out seen, sightlength))
+      {
+        if (seen.collider.tag == "Book")
+        {
+          if (selectedObj != null && selectedObj == seen.transform.gameObject)
+          {
+            exitButton.SetActive(true);
+            bookRecipes.SetActive(true);
+          }
+        }
+      }
+    }
+}
