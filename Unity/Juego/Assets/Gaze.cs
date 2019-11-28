48a9a3a3d24eca5217282e42e1388d129bc6b69c
Paul Huguet <p.huguet@horizonprevention.fr>
2019-11-28 10:37:57 +0100

    psusha

---
+++ Unity/Juego/Assets/Gaze.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    public float sightlength = 100f;
+    public GameObject selectedObj;
+    public float hoverforwardDistance = .5f;
+
+    void FixedUpdate()
+    {
+      RaycastHit seen;
+      Ray raydirection = new Ray(transform.position, transform.forward);
+      if (Physics.Raycast(raydirection, out seen, sightlength))
+      {
+        if (seen.collider.tag == "Button")
+        {
+          if (selectedObj != null && selectedObj != seen.transform.GameObject)
+          {
+            GameObject hitObject = seen.transform.gameObject;
+            Debug.Log("EH OH LA");
+            SceneManager.LoadScene("SampleScene");
+          }
+          selectedObj = seen.transform.gameObject;
+        }
+      }
+    }
+}
